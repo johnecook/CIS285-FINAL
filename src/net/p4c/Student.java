0eee36e6b6c481724c96683cd520f961ceda9d5e
John Cook <cookj@pickens.k12.al.us>
2021-04-20 12:23:53 -0500

    Initial commit

--- src/net/p4c/Student.java
+++ src/net/p4c/Student.java
@@ -1,9 +1,9 @@
 package net.p4c;
 
 public class Student {
+    private int studentID;
     private String firstName;
     private String lastName;
-    private Address address;
     private int[] grades;
     private int averageGrade;
     private String letterGrade;
